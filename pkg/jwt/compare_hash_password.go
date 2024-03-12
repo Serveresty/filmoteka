5b9be5bc1fef1c87bc2944d0d79eabbeab1b021f
Serveresty <cs.go.12228@gmail.com>
2024-03-13 02:37:32 +0300

    new func compare hash and password

---
+++ pkg/jwt/compare_hash_password.go
@@ -0,0 +1,11 @@
+package jwt
+
+import "golang.org/x/crypto/bcrypt"
+
+func ComparePasswordHash(password, hash string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+}
