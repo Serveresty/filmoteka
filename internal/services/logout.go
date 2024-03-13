bf2549085c197fff0c195a0a6a382087e0451033
Serveresty <cs.go.12228@gmail.com>
2024-03-13 03:27:42 +0300

    quick fix

--- internal/services/logout.go
+++ internal/services/logout.go
@@ -1,8 +1,25 @@
 package services
 
-import "net/http"
+import (
+	"filmoteka/pkg/jwt"
+	"net/http"
+)
 
 func Logout(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	token := r.Header.Get("Authorization")
+	path := r.URL.Path
+	status, err := jwt.CheckAuthorization(token, path)
+	if err != nil {
+		w.WriteHeader(status)
+		w.Write(err)
+		return
+	}
 	r.Header.Del("Authorization")
-	http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("token removed"))
 }
