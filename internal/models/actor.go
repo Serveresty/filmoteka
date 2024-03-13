d07fd06ccd7fabbb725bdb2ade5ddbd0eb25ff86
Serveresty <cs.go.12228@gmail.com>
2024-03-13 21:32:38 +0300

    actor, film struct

---
+++ internal/models/actor.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Actor struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Gender    rune      `json:"gender"`
+	BirthDate time.Time `json:"birth_date"`
+	Films     []Film    `json:"films"`
+}
---
+++ internal/models/film.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Film struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	ReleaseDate time.Time `json:"release_date"`
+	Rate        float64   `json:"rate"`
+	Actors      []Actor   `json:"actors"`
+}
