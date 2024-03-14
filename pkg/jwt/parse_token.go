34bfe5c116e02bf3b6615f94907475fedfc36fc6
Serveresty <cs.go.12228@gmail.com>
2024-03-14 04:26:23 +0300

    fixed an issue with parse jwt token

--- pkg/jwt/parse_token.go
+++ pkg/jwt/parse_token.go
@@ -12,17 +12,23 @@ import (
 func ParseToken(token string) (*models.JWTClaims, error) {
 	tokenArr := strings.Split(token, " ")
 	if len(tokenArr) < 2 {
-		return nil, fmt.Errorf("error wrong token")
+		return nil, fmt.Errorf("error wrong token signature")
 	}
 
 	tokenString := tokenArr[1]
-	secretKey := os.Getenv("SECRET_KEY")
 
 	currentToken, err := jwt.ParseWithClaims(tokenString, &models.JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return secretKey, nil
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("SECRET_KEY")), nil
 	})
 	if err != nil {
 		return nil, err
+	}
+
+	if !currentToken.Valid {
+		return nil, fmt.Errorf("error token is not valid")
 	}
 
 	claims, ok := currentToken.Claims.(*models.JWTClaims)
