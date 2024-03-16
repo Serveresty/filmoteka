492ec2e5ba72f4c14503b9d6aa75f82c304350f5
Serveresty <cs.go.12228@gmail.com>
2024-03-17 02:47:56 +0300

    created logout logs

--- internal/services/logout.go
+++ internal/services/logout.go
@@ -2,11 +2,16 @@ package services
 
 import (
 	"filmoteka/pkg/jwt"
+	"filmoteka/pkg/logger"
 	"net/http"
 )
 
 func Logout(w http.ResponseWriter, r *http.Request) {
+
+	logger.InfoLogger.Println("Handling " + r.Method + " request for: " + r.URL.Path)
+
 	if r.Method != "POST" {
+		logger.InfoLogger.Println("Invalid request method: " + r.Method + ", expected POST for URL: " + r.URL.Path)
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
@@ -15,10 +20,17 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	status, err := jwt.CheckAuthorization(token, path)
 	if err != nil {
+		logger.InfoLogger.Println(
+			r.Method + " | " + r.URL.Path +
+				" | Status: " + http.StatusText(status) +
+				" | Details: " + string(err[:]))
+
 		w.WriteHeader(status)
 		w.Write(err)
 		return
 	}
+
+	logger.InfoLogger.Println(r.Method + " | " + r.URL.Path + " | Status: " + http.StatusText(http.StatusOK) + " | Detail: token removed")
 	r.Header.Del("Authorization")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("token removed"))
