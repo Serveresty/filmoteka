d938876b29f1336a18364943704aaca2772d705d
Serveresty <cs.go.12228@gmail.com>
2024-03-17 02:26:33 +0300

    add logger for GET /sign-up

--- internal/services/sign_up.go
+++ internal/services/sign_up.go
@@ -5,11 +5,15 @@ import (
 	"filmoteka/internal/database"
 	"filmoteka/internal/models"
 	"filmoteka/pkg/jwt"
+	"filmoteka/pkg/logger"
 	"net/http"
 )
 
 func SignUp(w http.ResponseWriter, r *http.Request) {
+	logger.InfoLogger.Println("Handling " + r.Method + " request for: " + r.URL.Path)
+
 	if r.Method != "GET" {
+		logger.InfoLogger.Println("Invalid request method: " + r.Method + ", expected GET for URL: " + r.URL.Path)
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
@@ -18,11 +22,13 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	status, err := jwt.CheckAuthorization(token, path)
 	if err != nil {
+		logger.InfoLogger.Println(r.Method + " " + r.URL.Path + " Status: " + http.StatusText(status) + " Details: " + string(err[:]))
 		w.WriteHeader(status)
 		w.Write(err)
 		return
 	}
 
+	logger.InfoLogger.Println(r.Method + " " + r.URL.Path + " Status: " + http.StatusText(status))
 	w.WriteHeader(status)
 }
 
