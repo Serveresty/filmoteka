7327927489db966701e08092fbf86c03aec199e8
Serveresty <cs.go.12228@gmail.com>
2024-03-16 04:23:00 +0300

    func + endpoint for GET actors and films for

---
+++ internal/database/get_actors.go
@@ -0,0 +1,60 @@
+package database
+
+import (
+	"filmoteka/internal/models"
+	"fmt"
+)
+
+func GetActors() ([]models.ActorToFilm, error) {
+	rows, err := DB.Query(`
+	SELECT actors.*, films.* 
+	FROM actors 
+	LEFT JOIN actors_films ON actors.actor_id = actors_films.actor_id 
+	LEFT JOIN films ON actors_films.film_id = films.film_id
+	WHERE films.film_id IS NOT NULL
+	
+	UNION ALL
+	
+	SELECT actors.*, 0 AS film_id, '' AS name, '' AS description, '0001-01-01' AS release_date, 0.0 AS rate 
+	FROM actors 
+	LEFT JOIN actors_films ON actors.actor_id = actors_films.actor_id 
+	LEFT JOIN films ON actors_films.film_id = films.film_id
+	WHERE actors.actor_id NOT IN (SELECT actor_id FROM actors_films);`)
+	if err != nil {
+		return nil, err
+	}
+
+	var actors = make(map[models.Actor][]models.Film)
+	for rows.Next() {
+		var film models.Film
+		var actor models.Actor
+		var gender string
+		err = rows.Scan(&actor.ID, &actor.Name, &gender, &actor.BirthDate, &film.ID, &film.Title, &film.Description, &film.ReleaseDate, &film.Rate)
+		if err != nil {
+			return nil, err
+		}
+
+		actor.Gender = gender
+
+		if val, ok := actors[actor]; ok {
+			val = append(val, film)
+			actors[actor] = val
+		} else {
+			actors[actor] = []models.Film{film}
+		}
+	}
+	if err := rows.Err(); err != nil {
+		fmt.Println(err)
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	var result []models.ActorToFilm
+	for act, fil := range actors {
+		var res models.ActorToFilm
+		res.Actors = act
+		res.Movies = fil
+		result = append(result, res)
+	}
+	return result, nil
+}
--- internal/services/actors.go
+++ internal/services/actors.go
@@ -1,13 +1,55 @@
 package services
 
-import "net/http"
+import (
+	"encoding/json"
+	"filmoteka/internal/database"
+	"filmoteka/pkg/jwt"
+	"net/http"
+)
 
 func GetActors(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/actors" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 
-}
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
-func GetActorsByFilter(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	path := r.URL.Path
+	status, err := jwt.CheckAuthorization(token, path)
+	if err != nil {
+		w.WriteHeader(status)
+		w.Write(err)
+		return
+	}
 
+	_, err1 := jwt.ParseToken(token)
+	if err1 != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("error not valid token"))
+		return
+	}
+
+	actrs, err1 := database.GetActors()
+	if err1 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error while getting actors"))
+		return
+	}
+
+	jsonResp, err1 := json.Marshal(actrs)
+	if err1 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error while marhal actors"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResp)
 }
 
 func AddNewActor(w http.ResponseWriter, r *http.Request) {
